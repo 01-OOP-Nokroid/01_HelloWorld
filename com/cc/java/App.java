78f3f42c162baa3c195e626428ed9f8832245955
Nekroid <philipleinung@aol.de>
2024-01-23 12:48:15 +0100

    Strings OOP

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,10 +1,10 @@
 package com.cc.java;
 
-public class App 
+public class App
 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hallo Welt!");
     }
     
 }
